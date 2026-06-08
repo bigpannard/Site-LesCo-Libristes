f869b02943b6bf8dae391e3d540c45f43715a76a
Emmanuel Blancquart <big_pannard@mgn.com>
2026-06-08 14:45:49 +0200

    Version 0

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', function() {
+    const isExpanded = this.getAttribute('aria-expanded') === 'true';
+    this.setAttribute('aria-expanded', !isExpanded);
+    menu.setAttribute('aria-expanded', !isExpanded);
+
+    // Toggle la classe pour l'animation
+    menu.classList.toggle('active');
+    });
+});
+
+document.addEventListener('click', function(e) {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.nav-links');
+    if (!menu.contains(e.target) && !menuToggle.contains(e.target)) {
+        menuToggle.setAttribute('aria-expanded', 'false');
+        menu.setAttribute('aria-expanded', 'false');
+        menu.classList.remove('active');
+    }
+});
